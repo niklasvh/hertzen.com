691a6e877c09a6c192281e086146c1458ae152cc
Niklas von Hertzen <niklasvh@gmail.com>
2014-11-24 00:32:12 +0200

    Begin react-flux-bacon slides

---
+++ slides/react-flux-bacon/examples/example1.js
@@ -0,0 +1,25 @@
+var TodoItem = React.createClass({
+    render: function() {
+        return <div className={this.props.complete === true ? "complete" : "incomplete"}>
+            {this.props.children}
+        </div>;
+    }
+});
+
+var TodoList = React.createClass({
+    render: function() {
+        var list = this.props.items.map(item => <TodoItem complete={item.complete}>{item.text}</TodoItem>);
+        return <div><strong>Item count: {list.length}</strong> {list}</div>;
+    }
+});
+
+var data = [
+    {text: "First item", complete: false},
+    {text: "Second item", complete: true},
+    {text: "Third <script>window.alert('xss');</script> item", complete: false}
+];
+
+React.render(
+    <TodoList items={data} />,
+    document.getElementById('example1')
+);
